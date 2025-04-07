845bfe018a866de8af1a5039073850b7f9de02c2
Midas van Soest <midasvsoest@gmail.com>
2025-04-07 17:58:21 +0200

    Create and complete  class Quizz, getQuestions, moveToNextQuestion, shuffleQuestion, checkAnswer, hasEnded and filterQuestionsByDifficulty

--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,53 @@
 class Quiz {
     // YOUR CODE HERE:
     //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+    constructor (questions, timeLimit, timeRemaining){
+        this.questions = questions;
+        this.timeLimit = timeLimit;
+        this.timeRemaining = timeRemaining;
+        this.correctAnswers = 0;
+        this.currentQuestionIndex = 0;
+    }
 
-    // 2. getQuestion()
+    getQuestion(){
+        return this.questions[this.currentQuestionIndex];
+    }
     
-    // 3. moveToNextQuestion()
+    moveToNextQuestion(){
+        this.currentQuestionIndex++;
+    }
 
-    // 4. shuffleQuestions()
+    shuffleQuestions(){
+        for (let i = this.questions.length - 1; i > 0; i--){
+            let j = Math.floor(Math.random() *(i + 1));
+            [this.questions[i], this.questions[j]] = [this.questions[j], this.questions[i]];
+           }
+    }
 
-    // 5. checkAnswer(answer)
+    checkAnswer(answer){
+        if(answer === this.questions[this.currentQuestionIndex].answer){
+            this.correctAnswers++;
+        }
+    }
 
-    // 6. hasEnded()
+    hasEnded(){
+        /*if(this.currentQuestionIndex < this.questions.length){
+            return false;
+        }
+        return true;*/
+
+        return this.currentQuestionIndex === this.questions.length;
+    }
+
+    filterQuestionsByDifficulty(difficulty){
+        if (difficulty === 1|| difficulty === 2 || difficulty === 3) {
+            this.questions = this.questions.filter(selectedLevel => selectedLevel.difficulty === difficulty);
+        }
+        
+    }
+
+    averageDifficulty(){
+
+    }
 }
+
